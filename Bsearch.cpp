16ddb4a97823d4e4a5a103291024b300e009d82d
LeValery <55252769+LeValery@users.noreply.github.com>
2019-09-12 21:16:01 +0300

    Add files via upload

---
+++ Bsearch.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void bsearch(int* array, int x, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (array[i] < x)
+		{
+			cout << "Index: " << i;
+			break;
+		}
+	}
+}
+int main()
+{
+	int n;
+	cout << "Number of elements: ";
+	cin >> n;
+	cout << "Input array: " << endl;
+	int* array = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >> array[i];
+	int X;
+	cout << "Input X: ";
+	cin >> X;
+	bsearch(array, X, n);
+	return 0;
+}
